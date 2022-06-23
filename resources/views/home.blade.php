d174cb1052a0f599852a6501e3c19ea635009737
DavideIsaia <davide.enjoy@hotmail.it>
2022-06-23 14:42:24 +0200

    aggiunto style.css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Hello Laravel</title>
+  <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-  <header>
-    <ul>
-      <li><a href="{{ route('home') }}">Home</a></li>
-      <li><a href="{{ route('profile') }}">Profilo utente</a></li>
+  <div class="container">
+    <header>
+      <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('profile') }}">Profilo utente</a></li>
+      </ul>
+    </header>
+    <h1>Ciao {{ $name }}, Bentornato!</h1>
+    <h2>La tua Wishlist comprende:</h2> 
+    <ul class="cart">
+      @foreach ($cart as $item)
+        <li><strong>{{ $item['name'] }}</strong>: € {{ $item['price']}}</li>        
+      @endforeach
     </ul>
-  </header>
-  <h1>Ciao {{ $name }}, Bentornato!</h1>
-  <h3>La tua Wishlist comprende:</h3> 
-  <ul>
-    @foreach ($cart as $item)
-      <li><strong>{{ $item['name'] }}</strong>: € {{ $item['price']}}</li>        
-    @endforeach
-  </ul>
+</div>
 </body>
 </html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,14 +5,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>User Profile</title>
+  <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-  <header>
-    <ul>
-      <li><a href="{{ route('home') }}">Home</a></li>
-      <li><a href="{{ route('profile') }}">Profilo utente</a></li>
-    </ul>
-  </header>
-  <h2>Profilo di {{ $name }} {{ $lastname }}</h2>  
+  <div class="container">
+    <header>
+      <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('profile') }}">Profilo utente</a></li>
+      </ul>
+    </header>
+    <h2>Profilo di {{ $name }} {{ $lastname }}</h2>  
+  </div>
 </body>
 </html>
